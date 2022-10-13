87735890db5181633b17112e5d3c90ed595741b3
Shay001001 <114077671+Shay001001@users.noreply.github.com>
2022-10-14 00:31:56 +0100

    did the thing againn

---
+++ My project/Assets/Challenge 2/Scripts/OpenLink.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    public void OpenChannel(){
+        Application.OpenURL("https://github.com/Shay001001/Unit_2_Unity_B00146133");
+    }
+}
